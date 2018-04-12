f05afd45bf97ae82ba7a2a132230950a9a95ac41
ParkAnnLee <anpuli827@gmail.com>
2018-04-12 20:53:07 +0800

    added fab

--- app/src/main/java/com/example/anpu/circles/utilities/ScrollAwareFabBehavior.java
+++ app/src/main/java/com/example/anpu/circles/utilities/ScrollAwareFabBehavior.java
@@ -4,25 +4,64 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
 import android.view.View;
 
-import java.util.jar.Attributes;
-
 /**
  * Created by anpu on 2018/3/17.
+ * Dirted by wenhe on 2018/4/12.
  */
 // TODO: behavior not working
 public class ScrollAwareFabBehavior extends FloatingActionButton.Behavior {
+//
+//    private int toolbarHeight;
+//
+//    public ScrollAwareFabBehavior(Context context, AttributeSet attrs) {
+//        super();
+//        this.toolbarHeight = getToolbarHeight(context);
+//    }
+
+//    @Override
+//    public boolean layoutDependsOn(CoordinatorLayout parent, FloatingActionButton child, View dependency) {
+//        return super.layoutDependsOn(parent, child, dependency) || (dependency instanceof AppBarLayout);
+//    }
+//
+//    @Override
+//    public boolean onDependentViewChanged(CoordinatorLayout parent, FloatingActionButton child, View dependency) {
+//        boolean va = super.onDependentViewChanged(parent, child, dependency);
+//
+//        if (dependency instanceof AppBarLayout) {
+//            CoordinatorLayout.LayoutParams lp = (CoordinatorLayout.LayoutParams) child.getLayoutParams();
+//            int fabBottomMargin = lp.bottomMargin;
+//            int distnaceScroll = child.getHeight() + fabBottomMargin;
+//            float ratio = (float) dependency.getY() / (float) toolbarHeight;
+//            child.setTranslationY(-distnaceScroll * ratio);
+//        }
+//        return va;
+//    }
+//
+//    public static int getToolbarHeight(Context context) {
+//        final TypedArray stypledAttributes = context.getTheme().obtainStyledAttributes(new int[]{R.attr.actionBarSize});
+//        int toolbarHeight = (int) stypledAttributes.getDimension(0, 0);
+//        stypledAttributes.recycle();
+//
+//        return toolbarHeight;
+//    }
+
+
+    public ScrollAwareFabBehavior() {
+    }
 
     public ScrollAwareFabBehavior(Context context, AttributeSet attrs) {
-        super();
+        super(context, attrs);
     }
 
     @Override
     public boolean onStartNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull FloatingActionButton child, @NonNull View directTargetChild, @NonNull View target, int axes, int type) {
-        return super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, axes, type);
+        return axes == ViewCompat.SCROLL_AXIS_VERTICAL || super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, axes, type);
     }
+
 
     @Override
     public void onNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull FloatingActionButton child, @NonNull View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, int type) {
